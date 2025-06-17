ca80181be2084f393d90221e703edd91483320c9
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-17 16:26:39 +0000

    updated url

--- inventorymgtsystem/SMENNS/urls.py
+++ inventorymgtsystem/SMENNS/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     
-    path("", views.sales_view, name='sales_view'),
     path("", views.smenns, name='gold_calculator'),
+    path("", views.sales_view, name='sales_view'),
     path("", views.send_email, name='send_email'),
-    path('admin/', admin.site.urls),
+    
 
     
 ]
